29489699b460e077f45958060ad33153b144ecdc
garcijo <garcijo94@gmail.com>
2015-10-14 12:17:26 -0700

    Screen update
    Made everything bigger

--- For the future/Rescue1.lua
+++ For the future/Rescue1.lua
@@ -14,10 +14,10 @@ local function setupmap()
 		grida = display.newImage("rescue_grid.png")
 		grida.anchorX=0
 		grida.anchorY=0
-		grida.x=119.17
-		grida.y=82.17
-		grida.height=900
-		grida.width=900
+		grida.x=43.01
+		grida.y=41.93
+		grida.height=993.04
+		grida.width=993.04
 		
 		
 		--one_loop
@@ -25,18 +25,18 @@ local function setupmap()
 		onel.anchorX=0
 		onel.anchorY=0
 		onel.x=1063.96
-		onel.y=613.61
-		onel.height=117.03
-		onel.width=709.42
+		onel.y=625
+		onel.height=133
+		onel.width=805
 		
 		--two_loop
 		twol = display.newImage("two_loop.png")
 		twol.anchorX=0
 		twol.anchorY=0
 		twol.x=1063.96
-		twol.y=741.17
-		twol.height=117.03
-		twol.width=709.42
+		twol.y=768
+		twol.height=133
+		twol.width=805
 		
 		
 		--three_loop
@@ -44,162 +44,171 @@ local function setupmap()
 		threel.anchorX=0
 		threel.anchorY=0
 		threel.x=1063.96
-		threel.y=868.57
-		threel.height=117.03
-		threel.width=709.42
+		threel.y=910
+		threel.height=133
+		threel.width=805
 		
 		--up_arrow
 		 upa = display.newImage("up_arrow.png")
 		upa.anchorX=0
 		upa.anchorY=0
-		upa.x=1190.95
-		upa.y=219.17
-		upa.height=106.04
-		upa.width=106.04
+		upa.x=1192
+		upa.y=186
+		upa.height=122
+		upa.width=122
 		
 		--down_arrow
 		downa = display.newImage("down_arrow.png")
 		downa.anchorX=0
 		downa.anchorY=0
-		downa.x=1311.31
-		downa.y=219.17
-		downa.height=106.04
-		downa.width=106.04
+		downa.x=1330
+		downa.y=186
+		downa.height=122
+		downa.width=122
 		
 		--left_arrow
 		lefta = display.newImage("left_arrow.png")
 		lefta.anchorX=0
 		lefta.anchorY=0
-		lefta.x=1551.8
-		lefta.y=219.17
-		lefta.height=106.04
-		lefta.width=106.04
+		lefta.x=1468
+		lefta.y=186
+		lefta.height=122
+		lefta.width=122
 		
 		--right_arrow
 		righta = display.newImage("right_arrow.png")
 		righta.anchorX=0
 		righta.anchorY=0
-		righta.x=1432.05
-		righta.y=219.17
-		righta.height=106.04
-		righta.width=106.04
+		righta.x=1606
+		righta.y=186
+		righta.height=122
+		righta.width=122
 		
 		--one_button
 		oneb = display.newImage("one_button.png")
 		oneb.anchorX=0
 		oneb.anchorY=0
-		oneb.x=1251.21
-		oneb.y=346.86
-		oneb.height=106.04
-		oneb.width=106.04
+		oneb.x=1261
+		oneb.y=332
+		oneb.height=122
+		oneb.width=122
 		
 		--two_button
 		twob = display.newImage("two_button.png")
 		twob.anchorX=0
 		twob.anchorY=0
-		twob.x=1371.77
-		twob.y=346.86
-		twob.height=106.04
-		twob.width=106.04
+		twob.x=1399
+		twob.y=332
+		twob.height=122
+		twob.width=122
 		
 		--three_button
 		threeb = display.newImage("three_button.png")
 		threeb.anchorX=0
 		threeb.anchorY=0
-		threeb.x=1492.56
-		threeb.y=346.86
-		threeb.height=106.04
-		threeb.width=106.04
+		threeb.x=1538
+		threeb.y=332
+		threeb.height=122
+		threeb.width=122
 		
 		--start_button
 		start= display.newImage("start_button.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=123
-		start.y=918
-		start.height=60
-		start.width=223
+		start.x=46
+		start.y=968
+		start.height=66
+		start.width=246
 
 		--wall_a
 		start= display.newImage("locked_door_horizontal.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=181
-		start.y=305
+		start.x=110
+		start.y=288
 		start.height=10
-		start.width=110.5
+		start.width=124
 
 		--wall_b
 		start= display.newImage("locked_door_horizontal.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=408
-		start.y=305
-		start.height=10
-		start.width=110.5
+		start.x=359
+		start.y=288
+		start.height=11
+		start.width=124
 
 		--wall_c
 		start= display.newImage("locked_door_horizontal.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=621
-		start.y=305
-		start.height=10
-		start.width=110.5
+		start.x=598
+		start.y=288
+		start.height=11
+		start.width=124
 
 		--wall_d
 		start= display.newImage("locked_door_horizontal.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=842
-		start.y=305
-		start.height=10
-		start.width=110.5
+		start.x=839
+		start.y=288
+		start.height=11
+		start.width=124
 
 		--wall_f
 		start= display.newImage("locked_door_horizontal.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=408
-		start.y=527
-		start.height=10
-		start.width=110.5
+		start.x=359
+		start.y=533
+		start.height=11
+		start.width=124
 
 		--wall_j
 		start= display.newImage("locked_door_horizontal.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=408
-		start.y=750
-		start.height=10
-		start.width=110.5
+		start.x=359
+		start.y=777
+		start.height=11
+		start.width=124
 
 		--wall_7
 		start= display.newImage("locked_door_vertical.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=341
-		start.y=590
-		start.height=110.5
-		start.width=10
+		start.x=290
+		start.y=602
+		start.height=124
+		start.width=11
 
 		--wall_8
 		start= display.newImage("locked_door_vertical.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=564
-		start.y=590
-		start.height=110.5
-		start.width=10
+		start.x=534
+		start.y=602
+		start.height=124
+		start.width=12
 
 		--scientist
 		start= display.newImage("scientist.png")
 		start.anchorX=0
 		start.anchorY=0
-		start.x=859
-		start.y=363
-		start.height=126
-		start.width=126
+		start.x=843
+		start.y=348
+		start.height=140
+		start.width=140
+
+		--robot
+		start= display.newImage("robot.png")
+		start.anchorX=0
+		start.anchorY=0
+		start.x=109
+		start.y=819
+		start.height=140
+		start.width=140
 end 
 
 -- "scene:create()"
